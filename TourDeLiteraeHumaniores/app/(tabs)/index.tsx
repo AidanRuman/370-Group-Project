bb11c3ceb46b395e3d88f582dbae4aa521f89645
Aidan Ruman <aidanruman9@gmail.com>
2026-03-26 16:39:58 -0400

    Maps

--- TourDeLiteraeHumaniores/app/(tabs)/index.tsx
+++ TourDeLiteraeHumaniores/app/(tabs)/index.tsx
@@ -1,15 +1,30 @@
-import { Text, View } from "react-native";
+import React, { useRef } from "react";
+import { Text, View, StyleSheet } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+
+const CENTER_MAP = {
+  latitude: 37.15727,
+  longitude: 22.32834,
+  latitudeDelta: 10,
+  longitudeDelta: 10,
+};
 
 export default function Index() {
+  const mapRef = useRef<MapView>(null);
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <MapView
+        style={StyleSheet.absoluteFillObject}
+        ref={mapRef}
+        initialRegion={CENTER_MAP}
+        provider={PROVIDER_GOOGLE}
+      ></MapView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
