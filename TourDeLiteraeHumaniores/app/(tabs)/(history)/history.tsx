2a160b03eeb493063c470800661c69370ad017ea
Patrick Ruman <Patrickruman14@gmail.com>
2026-04-03 16:04:28 -0400

    FlatList added

--- TourDeLiteraeHumaniores/app/(tabs)/(history)/history.tsx
+++ TourDeLiteraeHumaniores/app/(tabs)/(history)/history.tsx
@@ -1,17 +1,47 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 
+interface Locations {
+  id: number;
+  name: string;
+}
+
+const DATA: Locations[] = [
+  {
+    id: 1,
+    name: "The Colchis, the land of the Golden Fleece and Medea",
+  },
+  {
+    id: 2,
+    name: "The Temple of Delphi",
+  },
+  {
+    id: 3,
+    name: "The River Styx",
+  },
+]
+
 const history = () => {
+
+  const displayLocation = ({ item }: {item: Locations}) => (
+  <View style={styles.Loc}>
+    <Text>{item.name}</Text>
+  </View>
+)
   return (
-      <View>
-        <Text style={{justifyContent: 'center', alignItems: 'center'}}>
-          history
-        </Text>
-      </View>
+    <FlatList
+      data={DATA}
+      renderItem={displayLocation}
+    />
   )
 }
 
 export default history
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  Loc: {
+      padding: 10,
+      backgroundColor: 'white',
+    }
+})
