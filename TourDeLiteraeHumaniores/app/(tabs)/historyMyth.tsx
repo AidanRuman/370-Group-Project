fb9ecbbd65776c07733c11a4e7067a85eb4d9c9f
Fynn <brickmaster2004@gmail.com>
2026-04-09 16:06:55 -0400

    just some stuff

--- TourDeLiteraeHumaniores/app/(tabs)/historyMyth.tsx
+++ TourDeLiteraeHumaniores/app/(tabs)/historyMyth.tsx
@@ -1,14 +1,69 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+
+interface Locations {
+  id: number;
+  name: string;
+}
+
+const DATA: Locations[] = [
+  {
+    id: 1,
+    name: "The Colchis, the land of the Golden Fleece and Medea",
+  },
+  {
+    id: 2,
+    name: "The Temple of Delphi",
+  },
+  {
+    id: 3,
+    name: "The River Styx",
+  },
+  {
+    id: 4,
+    name: "The Battle of Marathon",
+  },
+  {
+    id: 5,
+    name: "The Scicilian Expedition",
+  },
+  {
+    id: 6,
+    name: "The City of Carthage",
+  },
+]
 
 const history = () => {
+
+const Seperator = () => (
+    <View style={styles.separatorStyle}/>
+);
+
+  const displayLocation = ({ item }: {item: Locations}) => (
+  <View style={styles.Loc}>
+    <Text>{item.name}</Text>
+  </View>
+)
   return (
-    <View>
-      <Text>history</Text>
-    </View>
+    <FlatList
+      data={DATA}
+      renderItem={displayLocation}
+      ItemSeparatorComponent={Seperator}
+
+    />
   )
 }
 
 export default history
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  Loc: {
+      padding: 15,
+      backgroundColor: 'white',
+    },
+  separatorStyle: {
+    height: 1,
+    backgroundColor: 'black',
+  }
+})
