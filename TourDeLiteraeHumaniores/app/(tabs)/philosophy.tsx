1903ea5785dce6ef8aef4c4953e2074ec9a36ef2
Fynn <brickmaster2004@gmail.com>
2026-04-23 15:27:57 -0400

    setup changes this is technically up to date

--- TourDeLiteraeHumaniores/app/(tabs)/philosophy.tsx
+++ TourDeLiteraeHumaniores/app/(tabs)/philosophy.tsx
@@ -1,14 +1,70 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, FlatList } from 'react-native'
 import React from 'react'
 
+interface Locations {
+  id: number;
+  name: string;
+}
+
+const DATA: Locations[]= [
+  {
+    id: 1,
+    name: "Socarates/Plato",
+  },
+  {
+    id: 2,
+    name: "Aristotle",
+  },
+  {
+    id: 3,
+    name: "machiavelli",
+  },
+  {
+    id: 4,
+    name: "Cicero",
+  },
+  {
+    id: 5,
+    name: "Thomas Aquinas",
+  },
+  {
+    id: 6,
+    name: "Marcus Aurelius",
+  },
+]
+
+
+
 const philosophy = () => {
+
+  const displayLocation = ({ item }: {item: Locations}) => (
+  <View style={styles.Loc}>
+    <Text>{item.name}</Text>
+  </View>
+  )
+  const Seperator = () => (
+      <View style={styles.separatorStyle}/>
+  );
   return (
-    <View>
-      <Text>philosophy</Text>
-    </View>
+    <FlatList
+          data={DATA}
+          renderItem={displayLocation}
+          ItemSeparatorComponent={Seperator}
+    
+        />
   )
 }
 
 export default philosophy
 
-const styles = StyleSheet.create({})
+
+const styles = StyleSheet.create({
+  Loc: {
+      padding: 15,
+      backgroundColor: 'white',
+    },
+  separatorStyle: {
+    height: 1,
+    backgroundColor: 'black',
+  }
+})
